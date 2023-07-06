35772fb7fc7fed4f85ca485d8b5e99c8076bf988
Douglas Lyman <douglas.lyman@icloud.com>
2023-07-06 04:09:15 +0200

    Made a bunch of progress. The frame with the menu is mostly all done. Biggest focus will be the views from now on.

--- Artemis-Loader/ViewModels/MainViewModel.cs
+++ Artemis-Loader/ViewModels/MainViewModel.cs
@@ -10,8 +10,17 @@ namespace Artemis_Loader.ViewModels
     class MainViewModel : ObservableObject
     {
         public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ExtensionViewCommand { get; set; }
+        public RelayCommand BrowserViewCommand { get; set; }
+        public RelayCommand DeveloperViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
         public RelayCommand AboutViewCommand { get; set; }
+
         public HomeViewModel HomeView { get; set; }
+        public object ExtensionView { get; set; }
+        public object BrowserView { get; set; }
+        public object DeveloperView { get; set; }
+        public object SettingsView { get; set; }
         public object AboutView { get; set; }
 
         private object _currentView;
@@ -29,6 +38,18 @@ namespace Artemis_Loader.ViewModels
         {
             HomeView = new HomeViewModel();
             HomeViewCommand = new RelayCommand(o => CurrentView = HomeView);
+
+            ExtensionView = new object();
+            ExtensionViewCommand = new RelayCommand(o => CurrentView = ExtensionView);
+
+            BrowserView = new object();
+            BrowserViewCommand = new RelayCommand(o => CurrentView = BrowserView);
+
+            DeveloperView = new object();
+            DeveloperViewCommand = new RelayCommand(o => CurrentView = DeveloperView);
+
+            SettingsView = new object();
+            SettingsViewCommand = new RelayCommand(o => CurrentView = SettingsView);
 
             AboutView = new object();
             AboutViewCommand = new RelayCommand(o => CurrentView = AboutView);
